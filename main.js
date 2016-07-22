6f170a52d0bf96e7c945c893e71caae10b21d017
Un Known <jasonqan2004@gmail.com>
2016-07-22 16:24:03 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -64,10 +64,10 @@ function interpret(message) {
             mybot.sendMessage(message, "Here is my code: ", {file:"./main.js"});
         } else if (/^\/\/github$/i.test(message.content) && grand.github) {
             mybot.sendMessage(message, "Here is my github: \nhttps://github.com/Aplet123/The-Bot-of-Hope-and-Wisdom");
-        } else if (/^(n+o{2,}t+\s*)+$/i.test(message.content) && settings[message.server.id].noot) {
+        } else if (/^((n+o{2,}t+)|(h+i+)|(h+e+l{2,}o+)|🍳\s*)+$/i.test(message.content) && settings[message.server.id].noot) {
             mybot.sendMessage(message, message.content + " " + message.content);
         } else if (/^\/\/help$/i.test(message.content)) {
-            mybot.reply(message, "\n//calc [calculation] - Calculates the statement\n//info - Gives info for the bot\nnoot or any variation - Repeats your message twice\n//settings - Displays the current bot settings\n//anonymous [message] - Sends an anonyous message\n//code - Sends the code of the bot\n//github - Sends a link to the bot github\n//adm [adm command] - Performs an admin command. **Bot admins only**");
+            mybot.reply(message, "\n//calc [calculation] - Calculates the statement\n//info - Gives info for the bot\nnoot, hi, hello, 🍳, or any variation - Repeats your message twice\n//settings - Displays the current bot settings\n//anonymous [message] - Sends an anonyous message\n//code - Sends the code of the bot\n//github - Sends a link to the bot github\n//adm [adm command] - Performs an admin command. **Bot admins only**");
         } else if (/^\/\/settings$/i.test(message.content)) {
             mybot.reply(message, "\n```" + JSON.stringify(settings[message.server.id]) + "```");
         } else if (/^\/\/anonymous .+/i.test(message.content) && settings[message.server.id].anonymous && !del) {
